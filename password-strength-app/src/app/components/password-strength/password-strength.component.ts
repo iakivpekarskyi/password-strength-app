1fc5358a73695e5931b9d09d9c992582aa2c2ebe
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-03-08 21:52:25 +0100

    feat: initial commit

---
+++ password-strength-app/src/app/components/password-strength/password-strength.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-strength',
+  standalone: true,
+  imports: [],
+  templateUrl: './password-strength.component.html',
+  styleUrl: './password-strength.component.scss'
+})
+export class PasswordStrengthComponent {
+
+}
