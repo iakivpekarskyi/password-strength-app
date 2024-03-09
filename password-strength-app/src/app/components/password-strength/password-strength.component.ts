a97b40c81cbd5eaf20fd06a27715404465e28a8b
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-03-09 20:24:18 +0100

    feat: add functionality to the circles

--- password-strength-app/src/app/components/password-strength/password-strength.component.ts
+++ password-strength-app/src/app/components/password-strength/password-strength.component.ts
@@ -1,14 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-password-strength',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './password-strength.component.html',
-  styleUrl: './password-strength.component.scss',
+  styleUrls: ['./password-strength.component.scss'],
 })
 export class PasswordStrengthComponent {
+  password: string = '';
+  passwordStrength = ['gray', 'gray', 'gray'];
+  inputBorderColor: string = 'gray';
+
   checkPasswordStrength() {
-    // logic to check password strength
+    const length = this.password.length;
+    const hasLetters = /[a-zA-Z]+/.test(this.password);
+    const hasNumbers = /\d+/.test(this.password);
+    const hasSymbols = /\W+/.test(this.password);
+
+    switch (true) {
+      case length === 0:
+        this.passwordStrength = ['gray', 'gray', 'gray'];
+        break;
+
+      case length < 8:
+        this.passwordStrength = ['red', 'gray', 'gray'];
+        break;
+
+      case (hasLetters && hasNumbers) ||
+        (hasLetters && hasSymbols) ||
+        (hasNumbers && hasSymbols):
+        if (hasLetters && hasNumbers && hasSymbols) {
+          this.passwordStrength = ['green', 'green', 'green'];
+        } else {
+          this.passwordStrength = ['yellow', 'yellow', 'gray'];
+        }
+        break;
+
+      default:
+        this.passwordStrength = ['red', 'gray', 'gray'];
+        break;
+    }
+    this.inputBorderColor = this.passwordStrength[1];
+  }
+
+  resetPassword() {
+    this.password = '';
+    this.passwordStrength = ['gray', 'gray', 'gray'];
+    this.inputBorderColor = 'gray';
   }
 }
